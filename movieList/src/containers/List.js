01e15a1f0ca465a59f1a5c24a0db0ce05c16d30f
Treek <nbatre1@yahoo.com>
2019-12-28 16:48:19 -0600

    keep moving forward

---
+++ movieList/src/containers/List.js
@@ -0,0 +1,27 @@
+import React,{Component} from 'react';
+import Card from '../components/Card';
+
+
+class List extends Component{
+
+
+
+
+
+
+
+
+  render(){
+
+
+    return(
+        <div>
+            <Card />
+        </div>
+    )
+  }
+}
+
+
+
+export default List;
--- movieList/src/index.js
+++ movieList/src/index.js
@@ -1,1 +1,20 @@
-console.log("movieList");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from './containers/List';
+
+
+const App = () => {
+
+
+   return(
+       <>
+       <h1>Main area</h1>
+
+        <List/>
+       </>
+   )
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
