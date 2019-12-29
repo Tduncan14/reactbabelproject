93743bcbef1a1cef763aab028174e921b60ca1c3
Treek <nbatre1@yahoo.com>
2019-12-28 19:20:57 -0600

    keep moving forward

--- movieList/src/components/Card.js
+++ movieList/src/components/Card.js
@@ -1,22 +1,17 @@
 import React from 'react';
 
 
-const Card = ({movie}) =>{
 
-
-   console.log(movie);
-     return(
-        <>
-    <h2>{`#${movie.ranking} - ${movie.title} (${movie.year})`}</h2>
-    <img src={movie.img.src} at={movie.img.alt} width='200'/>
-
-     <p>{`Distributor: ${movie.distributor}`}</p>
- 
-     <p>{`Amount: ${movie.amount}1</p>
-        </>`}</p>
-        </>
-     )
-}
+const Card = ({ movie }) => {
+   return (
+      <div>
+          <h2>{`#${movie.ranking} - ${movie.title} (${movie.year})`}</h2>
+          <img src={movie.img.src} alt={movie.img.alt} width='200' />
+          <p>{`Distributor: ${movie.distributor}`}</p>
+          <p>{`Amount: ${movie.amount}`}</p>
+      </div>
+  );
+};
 
 
 
--- movieList/src/containers/List.js
+++ movieList/src/containers/List.js
@@ -2,22 +2,54 @@ import React,{Component} from 'react';
 import Card from '../components/Card';
 
 
+
+
+
 class List extends Component{
 
 
+  constructor(props){
+       super(props)
+
+       this.state={
+        data:[],
+        loading:true
+       }
+  }
 
 
+  async componentDidMount () {
 
+    const movies = await fetch('../../assets/db.json')
 
+    const moviesJson = await movies.json();
+
+    console.log('hey')
+
+     console.log(movies,"movies");
+    if(moviesJson){
+
+      this.setState({
+        data:moviesJson,
+        loading:false
+      })
+    }
+ }
 
 
   render(){
 
+    const {data,loading} = this.state;
 
-    return(
-        <div>
-            <Card />
-        </div>
+
+    if(loading){
+
+      <h1>Page is Loading</h1>
+    }
+
+
+    return data.map((movie) =>
+          <Card key={movie.id} movie={movie}/>
     )
   }
 }
