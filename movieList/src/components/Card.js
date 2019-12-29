21674c40e3295fa7632bd408659ebc82b6a5d954
Treek <nbatre1@yahoo.com>
2019-12-28 18:08:29 -0600

    keep moving forward

--- movieList/src/components/Card.js
+++ movieList/src/components/Card.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 
-const Card = () =>{
+const Card = ({movie}) =>{
 
+
+   console.log(movie);
      return(
+        <>
+    <h2>{`#${movie.ranking} - ${movie.title} (${movie.year})`}</h2>
+    <img src={movie.img.src} at={movie.img.alt} width='200'/>
 
-        <h2>movie#1</h2>
+     <p>{`Distributor: ${movie.distributor}`}</p>
+ 
+     <p>{`Amount: ${movie.amount}1</p>
+        </>`}</p>
+        </>
      )
 }
 
